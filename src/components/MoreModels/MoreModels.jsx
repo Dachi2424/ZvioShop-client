6dd6bf83e4321515c2d39829db5157b24bd41467
Dachi Darjania <dachidar19@gmail.com>
2026-07-14 17:40:05 +0400

    fixed bugs

--- src/components/MoreModels/MoreModels.jsx
+++ src/components/MoreModels/MoreModels.jsx
@@ -1,12 +1,17 @@
 import { useTranslation } from "react-i18next";
+import MacPower from "../../assets/macpower.jpg"
+import Grif from "../../assets/grif.jpg"
+import Energy from "../../assets/energy.webp"
+import Platin from "../../assets/platin.jpg"
+import Yigit from "../../assets/yigit.jpg"
 import "./MoreModels.scss";
 
 const extraModels = [
-  "Macpower",
-  "Grif",
-  "Energy Premium",
-  "Platin",
-  "Yigit Aku",
+  { name: "Macpower", image: MacPower },
+  { name: "Grif", image: Grif },
+  { name: "Energy Premium", image: Energy },
+  { name: "Platin", image: Platin },
+  { name: "Yigit Aku", image: Yigit },
 ];
 
 export default function MoreModels() {
@@ -14,17 +19,20 @@ export default function MoreModels() {
 
   return (
     <section className="section more-models">
-      <div className="container more-models__inner">
+      <div className="container">
         <div className="more-models__text">
           <h2 className="more-models__title">{t("moreModels_title")}</h2>
           <p className="more-models__subtitle">{t("moreModels_subtitle")}</p>
         </div>
 
-        <div className="more-models__list">
+        <div className="more-models__shelf">
           {extraModels.map((model) => (
-            <span className="more-models__pill" key={model}>
-              {model}
-            </span>
+            <div className="more-models__item" key={model.name}>
+              <div className="more-models__imgwrap">
+                <img src={model.image} alt={model.name} />
+              </div>
+              <span className="more-models__name">{model.name}</span>
+            </div>
           ))}
         </div>
 
